63bf16d05c8b54f15adf69542562a4eaa5bfedf2
Minh P. Dang <cunminh1405@gmail.com>
2022-12-30 23:54:11 +0200

    Finished: UI - wait for content

--- src/App.js
+++ src/App.js
@@ -9,36 +9,39 @@ import SkillPage from "./components/Skills";
 import ContactPage from "./components/ContactPage";
 
 const App = () => {
-  const ref = [useRef(null), useRef(null), useRef(null), useRef(null), useRef(null), useRef(null)]
   const pages = [<AboutMe/>, <ProjectPage/>, <EducationPage/>, <ExperiencePage/>, <SkillPage/>, <ContactPage/>]
-  const scrollTo = (ref) => {
-    window.scrollTo({
-        top: ref.current.offsetTop,
-        behavior: 'smooth',
+  const ref = useRef(Array(pages.length).fill(null))
+  const scrollTo = (current) => {
+    current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
     });
   };
 
   const sidebarHandler = (id) => {
-    try {scrollTo(ref[id]);}
+    try {scrollTo(ref.current[id]);}
     catch (e) {
       console.log(e);
     }
   }
 
   return (    
-  <div className="">
-      <Sidebar sidebarHandler={sidebarHandler} ></Sidebar>
-      <div className="snap-y absolute right-0 pl-8 pr-8 w-full-300 pb-8">
+  <div className="flex flex-row">
+      <div className="flex h-full flex-col overflow-y-auto justify-between snap-proximity snap-y absolute right-0 px-8 pb-4 w-full-300 ">
         {
           pages.map((page) => 
-          <div key={pages.indexOf(page)} ref={ref[pages.indexOf(page)]} className="pt-8 snap-center">
+          <div key={pages.indexOf(page)} ref={el => ref.current[pages.indexOf(page)] = el} className={pages.indexOf(page)===0?"pt-8 mb-4 snap-center":"my-4 snap-center"}>
             {page}
           </div>)
         }
       </div>
+      <div className="w-full h-screen p-8 top-0 bottom-0 left-0 right-0">
+        <Sidebar  sidebarHandler={sidebarHandler} ></Sidebar>
+      </div>
   </div>
   );
 };
+
 document.body.style.background = '#fafafa';
 
 export default App;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,61 +1,71 @@
+import LazyLoad from "react-lazy-load";
+
 const Sidebar = (props) => {
+
   return (
-      <>
       <div
-        className='sidebar fixed top-0 bottom-0 lg:left-0 p-2 min-w-300 overflow-y-auto text-center rounded-2xl m-8 bg-gray-900'
+        className='h-full p-2 max-w-[300px] text-center rounded-2xl bg-gray-900 flex flex-col'
       >
-        <div className="text-gray-100 text-xl">
-          <div className="p-2.5 mt-1 flex justify-between">
-            <p className="font-bold text-gray-200 text-[18px] ml-2">Minh Dang</p>
+        <div className="flex h-fit">
+          <LazyLoad className="w-full px-8 pt-8">
+            <img className="rounded-full" src="https://avatars.githubusercontent.com/u/37279565"></img>
+          </LazyLoad>
+          
+        </div>
+          <div className="text-gray-100 text-xl">
+            <div className="p-2.5 mt-1 flex justify-between">
+              <p className="font-bold text-gray-200 text-[18px] ml-2">Minh Dang</p>
+            </div>
+            <div className="my-2 bg-gray-600 h-[1px]"></div>
           </div>
-          <div className="my-2 bg-gray-600 h-[1px]"></div>
-        </div>
-        
-        <div onClick={() => props.sidebarHandler(0)}
-          className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-        >
-          <p   className="text-[15px] ml-2 text-gray-200 font-bold">About Me</p>
-        </div>
+      
 
-        <div
-          onClick={() => props.sidebarHandler(1)}
-          className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-        >
-          <p className="text-[15px] ml-2 text-gray-200 font-bold">Projects</p>
-        </div>
+          <div className="flex-1 max-h-full darkscrollbar overflow-y-auto">
+            <div onClick={() => props.sidebarHandler(0)}
+              className="p-2.5 mt-3 w-fill rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
+              >
+              <p   className="text-[15px] ml-2 text-gray-200 font-bold">About Me</p>
+            </div>
 
-        <div
-        onClick={() => props.sidebarHandler(2)}
-          className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-        >
-          <p className="text-[15px] ml-2 text-gray-200 font-bold">Education</p>
-        </div>
+            <div
+              onClick={() => props.sidebarHandler(1)}
+              className="p-2.5 mt-3  rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
+              >
+              <p className="text-[15px] ml-2 text-gray-200 font-bold">Projects</p>
+            </div>
 
-        <div
-          onClick={() => props.sidebarHandler(3)}
-          className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-        >
-          <p className="text-[15px] ml-2 text-gray-200 font-bold">Experience</p>
-        </div>
+            <div
+            onClick={() => props.sidebarHandler(2)}
+            className="p-2.5 mt-3  rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
+            >
+              <p className="text-[15px] ml-2 text-gray-200 font-bold">Education</p>
+            </div>
 
-        <div
-          onClick={() => props.sidebarHandler(4)}
-          className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-        >
-          <p className="text-[15px] ml-2 text-gray-200 font-bold">Education</p>
-        </div>
-        
+            <div
+              onClick={() => props.sidebarHandler(3)}
+              className="p-2.5 mt-3  rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
+              >
+              <p className="text-[15px] ml-2 text-gray-200 font-bold">Experience</p>
+            </div>
 
-        <div className="my-4 bg-gray-600 h-[1px]"></div>
+            <div
+              onClick={() => props.sidebarHandler(4)}
+              className="p-2.5 mt-3  rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
+              >
+              <p className="text-[15px] ml-2 text-gray-200 font-bold">Education</p>
+            </div>
+            
 
-        <div
-          onClick={() => props.sidebarHandler(5)}
-          className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-        >
-            <p className="text-[15px] ml-2 text-gray-200 font-bold">Contact</p>
-        </div>
+            <div className="my-4 mx-4 bg-gray-600 h-[1px]"></div>
+
+            <div
+              onClick={() => props.sidebarHandler(5)}
+              className="p-2.5 mt-3  rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
+              >
+                <p className="text-[15px] ml-2 text-gray-200 font-bold">Contact</p>
+            </div>
+          </div>
       </div>
-      </>
     ); 
 }
 
