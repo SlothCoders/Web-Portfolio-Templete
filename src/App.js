3d09634dd13c8942a165f2c2d6dc2afdc8f4fe2e
Minh P. Dang <cunminh1405@gmail.com>
2023-01-02 20:22:52 +0200

    Custom Domain

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useRef, useEffect } from "react";
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import AboutMe from "./components/AboutMe";
 import Sidebar from "./components/Sidebar";
 import ProjectPage from "./components/Projects";
@@ -37,22 +37,37 @@ const App = () => {
   useEffect(() => {sidebarHandler(0)})
   
   return (    
-  <div className="sm:flex sm:flex-row sm:py-0 pt-8 sm:overflow-y-auto h-full sm:h-screen">
-    <button onClick={() => scrollTop()} className="fixed sm:hidden p-4 m-2  active:shadow-2xl  active:outline-2  active:outline-blue-600  active:outline  active:bg-blue-200  h-fit min-w-8 w-fit right-0 bottom-0 rounded-md shadow-md bg-blue-300"> 
-      Scroll to top 
-    </button>
-    <div className="sm:min-w-[90px] sm:pl-8 sm:px-0 px-8 sm:py-8">
-      <Sidebar  sidebarHandler={sidebarHandler} ></Sidebar>
-    </div>
-    <div className="sm:grow h-full sm:flex-col sm:overflow-y-auto justify-between snap-proximity snap-y px-8">
-      {
-        pages.map((page) => 
-        <div key={pages.indexOf(page)} ref={el => ref.current[pages.indexOf(page)] = el} className={pages.indexOf(page)===0?"pt-8 mb-8 snap-start":"sm:my-8 mb-8 snap-center"}>
-          {page}
-        </div>)
-      }
-    </div>
-  </div>
+    <BrowserRouter>
+    <Routes>
+    
+    <Route path="/" element={
+        <div className="grid place-items-center h-screen">
+    <Link to="/portfolio">
+        <div className="h-fit w-fit p-4 active:shadow-2xl active:outline-2  active:outline-blue-600  active:outline  active:bg-blue-200 rounded-md shadow-md bg-blue-300">Access My Portfolio</div>
+    </Link>
+        </div>
+    }/>
+
+    <Route path="portfolio" element={
+      <div className="sm:flex sm:flex-row sm:py-0 pt-8 sm:overflow-y-auto h-full sm:h-screen">
+        <button onClick={() => scrollTop()} className="fixed sm:hidden p-4 m-2  active:shadow-2xl  active:outline-2  active:outline-blue-600  active:outline  active:bg-blue-200  h-fit min-w-8 w-fit right-0 bottom-0 rounded-md shadow-md bg-blue-300"> 
+          Scroll to top 
+        </button>
+        <div className="sm:min-w-[90px] sm:pl-8 sm:px-0 px-8 sm:py-8">
+          <Sidebar  sidebarHandler={sidebarHandler} ></Sidebar>
+        </div>
+        <div className="sm:grow h-full sm:flex-col sm:overflow-y-auto justify-between snap-proximity snap-y px-8">
+          {
+            pages.map((page) => 
+            <div key={pages.indexOf(page)} ref={el => ref.current[pages.indexOf(page)] = el} className={pages.indexOf(page)===0?"pt-8 mb-8 snap-start":"sm:my-8 mb-8 snap-center"}>
+              {page}
+            </div>)
+          }
+        </div>
+      </div>
+      } />
+    </Routes>
+    </BrowserRouter>
   );
 };
 
