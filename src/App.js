ba72908ab590352ce6c7b99f985a4882e3fee81a
Minh P. Dang <cunminh1405@gmail.com>
2023-01-02 17:57:13 +0200

    Styling

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ import ContactPage from "./components/ContactPage";
 
 const App = () => {
   const pages = [<AboutMe/>, <ProjectPage/>, <EducationPage/>, <ExperiencePage/>, <SkillPage/>, <ContactPage/>]
-  const ref = useRef(Array(pages.length).fill(null))
-  const homeRef = useRef(null);
-  
+  const ref = useRef(Array(pages.length).fill(null)) 
   
   const scrollTo = (current) => {
     current.scrollIntoView({
@@ -30,10 +28,7 @@ const App = () => {
   
   const scrollTop = () => {
     try {
-      homeRef.current.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      });
+        window.scrollTo({left: 0, top: 0, behavior: "smooth"})
     } catch (error) {
       console.log(error)
     }
@@ -42,14 +37,14 @@ const App = () => {
   useEffect(() => {sidebarHandler(0)})
   
   return (    
-  <div className="flex sm:flex-row sm:py-0 py-8 flex-col overflow-y-auto h-screen">
-    <button onClick={() => scrollTop()} className="sm:hidden p-4 m-2 hover:shadow-2xl hover:outline-2 hover:outline-blue-600 hover:outline hover:bg-blue-200 hover: h-fit min-w-8 w-fit right-0 bottom-0 absolute rounded-md shadow-md bg-blue-300"> 
+  <div className="sm:flex sm:flex-row sm:py-0 pt-8 sm:overflow-y-auto h-full sm:h-screen">
+    <button onClick={() => scrollTop()} className="fixed sm:hidden p-4 m-2  active:shadow-2xl  active:outline-2  active:outline-blue-600  active:outline  active:bg-blue-200  h-fit min-w-8 w-fit right-0 bottom-0 rounded-md shadow-md bg-blue-300"> 
       Scroll to top 
     </button>
-    <div ref={homeRef} className="sm:min-w-[90px] sm:pl-8 sm:px-0 px-8 sm:py-8">
+    <div className="sm:min-w-[90px] sm:pl-8 sm:px-0 px-8 sm:py-8">
       <Sidebar  sidebarHandler={sidebarHandler} ></Sidebar>
     </div>
-    <div className="sm:grow h-full sm:flex-col sm:overflow-y-auto justify-between snap-proximity snap-y px-8 pb-4">
+    <div className="sm:grow h-full sm:flex-col sm:overflow-y-auto justify-between snap-proximity snap-y px-8">
       {
         pages.map((page) => 
         <div key={pages.indexOf(page)} ref={el => ref.current[pages.indexOf(page)] = el} className={pages.indexOf(page)===0?"pt-8 mb-8 snap-start":"sm:my-8 mb-8 snap-center"}>
