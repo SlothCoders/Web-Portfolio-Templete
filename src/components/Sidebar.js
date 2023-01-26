719411d444ba30b43d9cce9b8382a76e2675ccd6
Minh P. Dang <cunminh1405@gmail.com>
2023-01-26 22:52:45 +0200

    Fixed: UI

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -21,62 +21,66 @@ const Sidebar = (props) => {
         </div>
     
 
-          <div className="flex-1 max-h-full darkscrollbar overflow-y-auto">
-            <div onClick={() => props.sidebarHandler(0)}
-              className="p-2.5 mt-3 rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
-              >
-              <p   className="text-[15px] ml-2 mt-2 text-gray-200 font-bold">About Me</p>
-            </div>
+          <div className="flex h-full darkscrollbar overflow-y-auto">
+            <div>
+              <div onClick={() => props.sidebarHandler(0)}
+                className="p-2.5 mt-3 rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
+                >
+                <p   className="text-[15px] ml-2 mt-2 text-gray-200 font-bold">About Me</p>
+              </div>
 
-            <div
-              onClick={() => props.sidebarHandler(1)}
+              <div
+                onClick={() => props.sidebarHandler(1)}
+                className="p-2.5 mt-3  rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
+                >
+                <p className="text-[15px] ml-2 mt-2 text-gray-200 font-bold">Education</p>
+              </div>
+
+              <div
+              onClick={() => props.sidebarHandler(2)}
               className="p-2.5 mt-3  rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
               >
-              <p className="text-[15px] ml-2 mt-2 text-gray-200 font-bold">Education</p>
-            </div>
+                <p className="text-[15px] ml-2 mt-2 text-gray-200 font-bold">Experience</p>
+              </div>
 
-            <div
-            onClick={() => props.sidebarHandler(2)}
-            className="p-2.5 mt-3  rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
-            >
-              <p className="text-[15px] ml-2 mt-2 text-gray-200 font-bold">Experience</p>
-            </div>
+              <div
+                onClick={() => props.sidebarHandler(3)}
+                className="p-2.5 mt-3  rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
+                >
+                <p className="text-[15px] ml-2 mt-2 text-gray-200 font-bold">Projects</p>
+              </div>
 
-            <div
-              onClick={() => props.sidebarHandler(3)}
-              className="p-2.5 mt-3  rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
-              >
-              <p className="text-[15px] ml-2 mt-2 text-gray-200 font-bold">Projects</p>
-            </div>
+              <div
+                onClick={() => props.sidebarHandler(4)}
+                className="p-2.5 mt-3  rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
+                >
+                <p className="text-[15px] ml-2 mt-2 text-gray-200 font-bold">Licenses & certifications</p>
+              </div>
 
-            <div
-              onClick={() => props.sidebarHandler(4)}
-              className="p-2.5 mt-3  rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
-              >
-              <p className="text-[15px] ml-2 mt-2 text-gray-200 font-bold">Licenses & certifications</p>
-            </div>
+              <div
+                onClick={() => props.sidebarHandler(5)}
+                className="p-2.5 mt-3  rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
+                >
+                <p className="text-[15px] ml-2 mt-2 text-gray-200 font-bold">Honors & awards</p>
+              </div>
+              
+              <div className="my-4 mx-4 bg-gray-600 h-[1px]"></div>
 
-            <div
-              onClick={() => props.sidebarHandler(5)}
-              className="p-2.5 mt-3  rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
-              >
-              <p className="text-[15px] ml-2 mt-2 text-gray-200 font-bold">Honors & awards</p>
-            </div>
-            
-            <div className="my-4 mx-4 bg-gray-600 h-[1px]"></div>
-
-            <div
-              onClick={() => props.sidebarHandler(6)}
-              className="p-2.5 mt-3  rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
-              >
-                <p className="text-[15px] ml-2 mt-2 text-gray-200 font-bold">Contact</p>
+              <div
+                onClick={() => props.sidebarHandler(6)}
+                className="p-2.5 mt-3  rounded-md px-4 cursor-pointer hover:bg-blue-600 text-white"
+                >
+                  <p className="text-[15px] ml-2 mt-2 text-gray-200 font-bold">Contact</p>
+              </div>
             </div>
         </div>
-        <div onClick={()=> window.open("https://github.com/minhdangphuoc/Web-Portfolio")} className="flex xl:flex-row flex-col justify-between items-center w-full bg-white rounded-md m-2 hover:bg-blue-50 hover:scale-[1.02]">
-          <img className="h-12 w-12 rounded-md p-2" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github logo"></img>
-          <p className="mb-0 grow sm:inline hidden text-center font-semibold">
-            Visit website repo
-          </p>
+        <div className="flex flex-1 items-end w-full">
+          <div onClick={()=> window.open("https://github.com/minhdangphuoc/Web-Portfolio")} className="flex flex-1 flex-nowrap xl:flex-row lg:flex-col sm:flex-col justify-between items-center w-full bg-white rounded-md m-2 hover:bg-blue-50 hover:scale-[1.02]">
+            <img className="h-12 w-12 rounded-md p-2" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github logo"></img>
+            <p className="mb-0 grow lg:hidden sm:hidden xl:inline text-center font-semibold">
+              Visit website repo
+            </p>
+          </div>
         </div>
       </div>
     ); 
